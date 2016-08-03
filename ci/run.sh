1cdf27c6e3581e9d63d5dc12d34eb10c8e7f7ef4
Karol Wozniak <karol.wozniak@email.com>
2016-08-04 00:18:54 +0200

    use encrypted key to fetch repo from github, enable gh-pages fetch only for master branch

--- ci/run.sh
+++ ci/run.sh
@@ -10,10 +10,12 @@ make && \
     export PATH=$PWD/bin:$PATH && \
     mkdir -p ./data && \
     mkdir -p ./streams && \
+    mkdir -p ./logs && \
     wget -q --directory-prefix=./data --accept=zip --input-file=http://wftp3.itu.int/av-arch/jctvc-site/bitstream_exchange/draft_conformance/RExt && \
     wget -q --directory-prefix=./data --accept=zip --input-file=http://wftp3.itu.int/av-arch/jctvc-site/bitstream_exchange/draft_conformance/HEVC_v1 && \
     find ./data -maxdepth 1 -type f -name '*.zip' | while read f; do unzip -qq -o $f -d ./data/`basename $f .zip`; done && \
-    find ./data -type f -name '*.bit' -or -name '*.bin' | while read f; do sha1sum $f; mv $f ./streams/`sha1sum $f | awk '{print $1}'`.`basename $f`; done && \
+    find ./data -type f -name '*.bit' -or -name '*.bin' | while read f; do sha1sum $f >> ./logs/streams.sha1 ; mv $f ./streams/`sha1sum $f | awk '{print $1}'`.`basename $f`; done && \
+    cat ./logs/streams.sha1 && \
     virtualenv tests && \
     source ./tests/bin/activate && \
     pip install pytest && \

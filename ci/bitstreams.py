02151afb2d7a3e0140525d728190cb5bcf4aa1a0
Wlodzimierz Lipert <wlodzimierz.lipert@contractor.qiagen.com>
2017-09-06 10:53:31 +0200

    Unmasked RExt streams for which fractional samples interpolation was fixed in last commit.

--- ci/bitstreams.py
+++ ci/bitstreams.py
@@ -8,7 +8,6 @@ extensions = ('.bit', '.bin')
 thismodule = sys.modules[__name__]
 IGNORE = [
     'dfab7514dc5803739837a111b14e63013a42210a', # Bitdepth_B_RExt_Sony_1_bin
-    '49a4fb424544847172b571aaa3f34cb2e54ad6cc', # GENERAL_12b_444_RExt_Sony_2_bit
     '82bae8f6d87e323a588cf25102cc60a4566e073a', # WAVETILES_RExt_Sony_2_bit
     'a6bc5efd7b495a1898b061a02296668b070ccbdb', # QMATRIX_A_RExt_Sony_1_bit
     'e760329a488e25b1e36cc6c1688b18ab6f1d8dba', # ExplicitRdpcm_B_BBC_2_bit
@@ -19,9 +18,6 @@ IGNORE = [
     '4ab746eb9c911e56f97db922784020def4e50c1a', # EXTPREC_MAIN_444_16_INTRA_16BIT_RExt_Sony_1_bit
     '947844980938d0d00ab4e658861b6d5217b68ff3', # GENERAL_16b_444_highThroughput_RExt_Sony_2_bit
     'ac9e6784aa7834cfa73734accdae7ae3ce05daa1', # ExplicitRdpcm_A_BBC_1_bit
-    '0fc7d8b534412a51d9e515a58e12a3a1be688010', # PERSIST_RPARAM_A_RExt_Sony_3_bit
-    '40699703214c1ef374c1e84ae973a9792606b76f', # GENERAL_10b_444_RExt_Sony_2_bit
-    'aa3cb828cc50e8156ef9c2446692be51d3cede33', # GENERAL_8b_444_RExt_Sony_2_bit
 ]
 
 def get_streams(path):
